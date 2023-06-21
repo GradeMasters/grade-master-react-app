b1d8235d44f6c79f033a9fcc6144a6e9eeb46d32
Joe Vandzura <vandzura.joe.e@gmail.com>
2023-06-21 11:37:06 -0500

    set up basic log in form

---
+++ src/Components/LogIn.js
@@ -0,0 +1,29 @@
+import React from "react";
+
+const LogIn = () => {
+  return (
+    <div>
+      <h1>Welcome to GradeMaster!</h1>
+      <h5>Log in to continue</h5>
+      <form>
+        <input placeholder="Email Address" type="text"></input>
+        <input placeholder="Password" type="password"></input>
+        <button type="submit">SIGN IN</button>
+      </form>
+      <h5>
+        Don't have an account? Click{" "}
+        <strong>
+          <a href="#">Register</a>
+        </strong>
+      </h5>
+      <h5>
+        Forgot Password? Click{" "}
+        <strong>
+          <a href="#">Reset</a>
+        </strong>
+      </h5>
+    </div>
+  );
+};
+
+export default LogIn;
